f787d0631940a4ab59c755aa71cadac3b8159318
webgenapp <87999663+webgenapp@users.noreply.github.com>
2021-07-26 21:23:07 +0200

    Create app using webgen.dev

---
+++ 20210726092303/app/.sequelize.config.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+module.exports = {
+  development: {
+    dialect: process.env.DATABASE_DIALECT,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    name: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+  },
+}
