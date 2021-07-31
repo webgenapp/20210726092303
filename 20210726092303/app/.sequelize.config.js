18d9bcae76716889a7c00dd257aac4bdcd716dda
EmmanuelM <meric.emmanuel@gmail.com>
2021-07-31 08:57:52 +0200

    Add production config

--- 20210726092303/app/.sequelize.config.js
+++ 20210726092303/app/.sequelize.config.js
@@ -11,4 +11,12 @@ module.exports = {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
   },
+  production: {
+    dialect: process.env.DATABASE_DIALECT,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    name: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+  }
 }
