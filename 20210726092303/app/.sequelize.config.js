9335f1e58f3999cdc5dd822b400c6964d336af9f
EmmanuelM <meric.emmanuel@gmail.com>
2021-07-31 10:03:07 +0200

    Use url for sequelize-cli production config

--- 20210726092303/app/.sequelize.config.js
+++ 20210726092303/app/.sequelize.config.js
@@ -12,11 +12,7 @@ module.exports = {
     password: process.env.DATABASE_PASSWORD,
   },
   production: {
-    dialect: process.env.DATABASE_DIALECT,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    name: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
+    url: process.env.DATABASE_URL,
+    dialect: 'psql',
   }
 }
